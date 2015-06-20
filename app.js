395c20fe7202eed895f230f4a98ea588d1f4ca05
Tyson Bailey <onaclov2000+github@gmail.com>
2015-06-20 06:26:03 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var response = {};
 
 io.on('connection', function(socket) {
-    console.log('a user connected');
-    app.get('*', function(req, res) {
-        io.emit('route_request', {
-            route: req.url
-        }); // Emit to everyone that is calling
-
-        socket.on('response', function(http_response, result) {
-            console.log(result);
-            http_response.send(result); // in theory return what a connected client returns
-        }.bind(null, res));
+    console.log("a new server is online");
+    socket.on('response', function(result) {
+        console.log(result);
+        if (response) {
+            response.send(result); // in theory return what a connected client returns;
+            response = {};
+        }
     });
+});
+
+
+app.get('*', function(req, res) {
+    console.log(req.url);
+    io.emit('route_request', {
+        route: req.url
+    }); // Emit to everyone that is calling
+
+    response = res;
 });
 
 http.listen(3000, function() {
